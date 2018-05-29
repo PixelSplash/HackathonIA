4cae06ecd547902277e95f6df0a0cb59868421c5
Briam Mora <bryam.car@gmail.com>
2018-05-28 19:50:11 -0600

    codigo python

---
+++ Automata.py
@@ -0,0 +1,12 @@
+import random
+
+class actor:
+    pid = 0
+    genero = 0
+    numero = 0
+
+    def __init__(self, ppid, pgenero, pnumero):
+        self.pid = ppid
+        self.genero = pgenero
+        self.numero = pnumero
+
