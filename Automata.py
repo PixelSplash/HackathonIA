a11e1b803a7ababdfb632cd6f4b8636fa3f64243
Briam Mora <bryam.car@gmail.com>
2018-05-28 20:00:51 -0600

    no message

--- Automata.py
+++ Automata.py
@@ -10,3 +10,67 @@ class actor:
         self.genero = pgenero
         self.numero = pnumero
 
+def Alejamiento(a1,a2):
+    pass
+def Prohibicion(a1,a2):
+    pass
+def Transgresion(a1,a2):
+    pass
+def Conocimiento(a1,a2):
+    pass
+def Informacion(a1,a2):
+    pass
+def Engano(a1,a2):
+    pass
+def Complicidad(a1,a2):
+    pass
+def Fechoria(a1,a2):
+    pass
+def Mediacion(a1,a2):
+    pass
+def Aceptacion(a1,a2):
+    pass
+def Partida(a1,a2):
+    pass
+def Prueba(a1,a2):
+    pass
+def Reaccionheroe(a1,a2):
+    pass
+def Regalo(a1,a2):
+    pass
+def Viaje(a1,a2):
+    pass
+def Lucha(a1,a2):
+    pass
+def Marca(a1,a2):
+    pass
+def Victoria(a1,a2):
+    pass
+def Enmienda(a1,a2):
+    pass
+def Regreso(a1,a2):
+    pass
+def Persecucion(a1,a2):
+    pass
+def Socorro(a1,a2):
+    pass
+def Regresoincognito(a1,a2):
+    pass
+def Fingimiento(a1,a2):
+    pass
+def Tareadificil(a1,a2):
+    pass
+def Cumplimiento(a1,a2):
+    pass
+def Reconocimiento(a1,a2):
+    pass
+def Desenmascaramiento(a1,a2):
+    pass
+def Transfiguracion(a1,a2):
+    pass
+def Castigo(a1,a2):
+    pass
+def Boda(a1,a2):
+    pass
+
+funciones = [Alejamiento,Prohibicion,Transgresion,Conocimiento,Informacion,Engano,Complicidad,Fechoria,Mediacion,Aceptacion,Partida,Prueba,Reaccionheroe,Regalo,Viaje,Lucha,Marca,Victoria,Enmienda,Regreso,Persecucion,Socorro,Regresoincognito,Fingimiento,Tareadificil,Cumplimiento,Reconocimiento,Desenmascaramiento,Transfiguracion,Castigo,Boda]
